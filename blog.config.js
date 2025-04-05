6c664a54ec701531eb9cd95467913b5c83be2c21
kitatsuk <125632852+dwouc150@users.noreply.github.com>
2025-04-06 01:16:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,11 +20,11 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '欢迎来到Grasser服务器',
+    '欢迎来到kitahub服务器',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Grasser', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'kitahub', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '公益，免费，自由', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://steamcommunity.com/profiles/76561199349965987/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Steam', // 网站关键词 英文逗号隔开
